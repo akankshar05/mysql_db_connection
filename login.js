971f174774550a8d47f125c16bc51834697d097f
Neeraj <stneeraj531@gmail.com>
2021-12-16 18:32:12 +0530

    sqli done

--- login.js
+++ login.js
@@ -1,6 +1,3 @@
-
-
-
 const mysql = require("mysql");
 const express = require("express");
 var path = require('path')
@@ -9,8 +6,6 @@ const encoder = bodyParser.urlencoded();
 
 const app = express();
 app.use("assests",express.static("assests"));
-// app.use()
-// app.use(express.static(path.join(__dirname, 'assets')));
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -33,19 +28,33 @@ app.get("/",function(req,res){
 app.post("/",encoder, function(req,res){
     var username = req.body.username;
     var password = req.body.password;
-    var sql = "inesrt into credentials values ?";
-    connection.query("select * from credentials where user_name = ? and user_pass = ?",[username,password],function(error,results,fields){
+    
+    var sql = "select * from credentials where user_name ='"+username+"'  and user_pass ='"+password+"'";
+
+    
+    // var sql = "inesrt into credentials values ?";
+    console.log(sql);
+         
+    connection.query(sql,[username,password],function(error,results,fields){
         if (results.length > 0) {
             console.log("old user");    
+            console.log(sql);
+            console.log(results);
+            console.log(results.length);
             res.redirect("/welcome");
             // console.log("new user");
         } else {
-            // res.redirect("/");
-            console.log("new user");
-            connection.query(sql,['null', username, password], function(err){
-                if (err) throw err
-            })
-            res.redirect("/welcome")
+            res.redirect("/wrong_pswd");
+            
+            // var newsql="insert into credentials values('null','"+username+"','"+password+"')"
+            // var newsql="insert into credentials values( null,' "+username+"','"+password+"')"
+            // console.log("new user");
+            // console.log(newsql);
+
+            // connection.query(newsql,[username, password], function(err){
+            //     if (err) throw err
+            // })
+            // res.redirect("/welcome")
         }
         res.end();
     })
@@ -54,6 +63,9 @@ app.post("/",encoder, function(req,res){
 // when login is success
 app.get("/welcome",function(req,res){
     res.sendFile(__dirname + "/welcome.html")
+})
+app.get("/wrong_pswd",function(req,res){
+    res.sendFile(__dirname + "/wrong_pswd.html")
 })
 
 
